3547d29a82325e1bb2f6c09d1260516910779ce0
Aurel-Charles <charlesaurelienpro@gmail.com>
2026-08-27 21:59:30 +0200

    add react router package add vitest package

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './tests/setup.js',
+  },
+});
